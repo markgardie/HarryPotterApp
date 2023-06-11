80f2ba8025c1d309e52d8d1754e0125ea7ed2755
markgardie <markgardie@users.noreply.github.com>
2023-06-11 18:01:56 +0300

    application class for hilt

---
+++ app/src/main/java/com/example/harrypotterapp/di/HarryPotterApplication.kt
@@ -0,0 +1,8 @@
+package com.example.harrypotterapp.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HarryPotterApplication: Application() {
+}
